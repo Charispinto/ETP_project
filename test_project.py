394dd391eb1c0e7820b39817b24a7e5fa2b7148d
Charispinto <Charispinto@users.noreply.github.com>
2023-12-14 00:13:18 +0530

    automated commit by submit50

--- test_project.py
+++ test_project.py
@@ -1,4 +1,4 @@
-from project import get_pdf_text, get_text_chunks
+from project import get_pdf_text, get_text_chunks,get_vectorstore
 
 
 def test_get_pdf_text():
@@ -11,3 +11,11 @@ def test_get_text_chunks():
     chunks = get_text_chunks(sample_text)
     assert len(chunks) > 0
     assert sample_text in chunks[0]
+
+def test_get_vectorstore():
+    sample_text = "This is a test pdf"
+    chunks = get_text_chunks(sample_text)
+    vector_store = get_vectorstore(chunks)
+    x = type(vector_store)
+    assert type(vector_store) == x
+
